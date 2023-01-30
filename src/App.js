2c3c77cf95788ec210bda78411da642cb732208d
krsy0411 <krsy0411@naver.com>
2023-01-30 13:50:08 +0900

    기본 환경설정 및 컴포넌트 구성요소 생성

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { CreateGroup } from "./components/CreateGroup";
+import { AddMembers } from "./components/AddMembers";
+import { ExpenseMain } from "./components/ExpenseMain";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<CreateGroup />} />
+      <Route path="/members" element={<AddMembers />} />
+      <Route path="/expense" element={<ExpenseMain />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/components/ExpenseMain.jsx
@@ -0,0 +1,17 @@
+export const ExpenseMain = () => {
+  return (
+    <div>
+      ExpenseMain Component
+      {/* left pane */}
+      <div>
+        {/* TODO : 비용 추가 form */}
+        {/* TODO : 정산 결과 컴포넌트 랜더링 */}
+      </div>
+      {/* right pane */}
+      <div>
+        {/* TODO : 그룹명 헤더 랜더링 */}
+        {/* TODO : 비용 리스트 컴포넌트 랜더링 */}
+      </div>
+    </div>
+  );
+};
